6f4a5f00e4c48c1fe47dd8cd53f415fd6e4b4d69
Pranay-26 <57473870+Pranay-26@users.noreply.github.com>
2020-02-03 12:22:30 +0530

    Update local_exec.tf

--- 04-Terraform/03-Ansible-Deploy/local_exec.tf
+++ 04-Terraform/03-Ansible-Deploy/local_exec.tf
@@ -30,7 +30,7 @@ provisioner "remote-exec" {
     inline = [
       "sudo apt-get update",
       "sudo apt-get install git -y",
-      "sudo apt-get install python -y",
+      "sudo apt-get install python sshpass -y",
     ]
   }
 }
