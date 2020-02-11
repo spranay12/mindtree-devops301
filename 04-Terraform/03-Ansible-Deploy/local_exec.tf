7c76697d0005d2ccc32620b3b1ccb0c2eecd8e6a
Pranay-26 <57473870+Pranay-26@users.noreply.github.com>
2020-02-11 23:39:11 +0530

    Update local_exec.tf

--- 04-Terraform/03-Ansible-Deploy/local_exec.tf
+++ 04-Terraform/03-Ansible-Deploy/local_exec.tf
@@ -7,24 +7,86 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
+}
+
+resource "aws_vpc" "my_network" {
+    cidr_block              = "10.10.0.0/16"
+    enable_dns_hostnames    = true
+    tags = {
+        Name = "spring-petclinic"
+    }
+}
+
+resource "aws_subnet" "pub_subnet" {
+    cidr_block              = "10.10.0.0/24"
+    vpc_id                  = "${aws_vpc.my_network.id}"
+    availability_zone       = "us-east-1a"
+    tags = {
+        Name = "spring-petclinic"
+    }
 
 }
 
-resource "aws_instance" "backend" {
-  ami                    = "ami-0f2cc133ca38673a8"
-  instance_type          = "t2.micro"
-  key_name               = "${var.key_name}"
-  vpc_security_group_ids = ["${var.sg-id}"]
+resource "aws_internet_gateway" "my_igw" {
+    vpc_id      = "${aws_vpc.my_network.id}"
+    tags = {
+        Name = "spring-petclinic"
+    }
 
 }
 
-resource "null_resource" "remote-exec-1" {
-    connection {
-    user        = "ubuntu"
-    type        = "ssh"
-    private_key = "${file(var.pvt_key)}"
-    host        = "${aws_instance.backend.public_ip}"
-  }
+resource "aws_route_table" "my_rt" {
+    vpc_id = "${aws_vpc.my_network.id}"
+
+    route {
+        cidr_block  = "0.0.0.0/0"
+        gateway_id  = "${aws_internet_gateway.my_igw.id}"
+    }
+
+    tags = {
+        Name = "spring-petclinic"
+    }
+
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = "${aws_subnet.pub_subnet.id}"
+  route_table_id = "${aws_route_table.my_rt.id}"
+}
+
+resource "aws_security_group" "my_sg" {
+    name            = "my_sg"
+    description     = "created from terraform"
+    vpc_id          = "${aws_vpc.my_network.id}"
+    ingress{
+        cidr_blocks = ["0.0.0.0/0"]
+        protocol    = "-1"
+        from_port   = "0"
+        to_port     = "0"
+    }
+    egress{
+        cidr_blocks = ["0.0.0.0/0"]
+        protocol    = "-1"
+        from_port   = "0"
+        to_port     = "0"
+    }
+    tags = {
+        Name = "spring-petclinic"
+    }
+}
+
+
+resource "aws_instance" "petclinic-web" {
+    ami                         = "ami-0f2cc133ca38673a8"
+    instance_type               = "t2.micro"
+    key_name                    = "${var.key_name}"
+    subnet_id                   = "${aws_subnet.pub_subnet.id}"
+    associate_public_ip_address = true
+    vpc_security_group_ids      = [ "${aws_security_group.my_sg.id}" ]
+    tags = {
+        Name = "spring-petclinic"
+    }
+
 }
 
 resource "null_resource" "ansible-main" {
@@ -34,8 +96,7 @@ provisioner "local-exec" {
         > jenkins-ci.ini;
         echo "[jenkins-ci]"| tee -a jenkins-ci.ini;
         export ANSIBLE_HOST_KEY_CHECKING=False;
-        echo "${aws_instance.backend.public_ip}" | tee -a jenkins-ci.ini;
-        ansible-playbook  --key=${var.pvt_key} -i jenkins-ci.ini ./ansible/04-Tomcat/web-playbook.yaml -u ubuntu -v
+        echo "${aws_instance.petclinic-web.public_ip}" | tee -a jenkins-ci.ini;
     EOT
 }
 }
